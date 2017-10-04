8b6e2b28831cfc84419540cebdca4c7596e84c57
abhishek47 <abhishekwani@hotmail.com>
2017-10-04 14:18:52 +0530

    Wiki Design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -232,8 +232,7 @@
 
       <!-- external javascripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="/js/jquery-ui.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
+<!-- <script src="/js/jquery-ui.min.js"></script> -->
 <!-- JS | jquery plugin collection for this theme -->
 <script src="/js/jquery-plugin-collection.js"></script>
 
@@ -366,6 +365,12 @@
 <script type="text/javascript" src="/js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
 <script type="text/javascript" src="/js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
 <script type="text/javascript" src="/js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+</script>
 
 
 
