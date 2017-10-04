aae1a104dd35366cf0d9a25f1ddf875f659ea56b
abhishek47 <abhishekwani@hotmail.com>
2017-10-04 10:54:00 +0530

    Wiki Design

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -6,12 +6,16 @@ use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    public function upload(Request $request)
-    {
-    	$src = $request->file('files')->store('uploads');
+    public function upload(Request $request) {
+	    $file = $request->file('files');
 
-    	$path = asset($src);
+	    $imageName = 'uploads/' . uniqid();
 
-    	return response(['src' => $path], 200);
-    }
+	    \Storage::disk('s3')->put($imageName, file_get_contents($file));
+	    \Storage::disk('s3')->setVisibility($imageName, 'public');
+
+	    $url = \Storage::disk('s3')->url($imageName);
+
+	    return response(["src" => $url ], 200);
+	}	
 }
