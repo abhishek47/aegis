f808d7b286fdfc9e0442eea8bb2a5909a2a3d59d
abhishek47 <abhishekwani@hotmail.com>
2017-10-29 20:32:47 +0530

    Favicons

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,8 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
-<!--
-<meta property="og:image" content="https://aegisacademy.co.in/images/bg/icon.png" />
--->
+
+<meta property="og:image" content="https://aegisacademy.co.in/images/apple-icon-180x180.png" />
 
 <meta property="og:description" content="Aegis Academy is started by young educators to provide high - quality education at affordable price. Almost every country conducts several exams to hunt extraordinary problem - solving skilled students.Olympiads are the most prestigious exam among them." />
 
