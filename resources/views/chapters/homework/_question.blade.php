7fb162b7ec15306a3d937f769b68ce844d491164
abhishek47 <abhishekwani@hotmail.com>
2017-11-20 17:08:47 +0530

    pages

--- resources/views/chapters/homework/_question.blade.php
+++ resources/views/chapters/homework/_question.blade.php
@@ -1,23 +1,18 @@
-<div class="course-single-item bg-white border-1px clearfix mb-30" style="width: 100%;">
-	<div class="course-details clearfix p-20 pt-15">
-
-                  <div class="course-top-part pull-left mr-40">
-                    <h5 class="mb-0">
-					    	Problem {{ $index+1 }} | 
-						    @if(!$homework->isSolved())		
-						    	Points : {{ $homework->points }}
-						    @else
-						    	Score : {{ $homework->userSolution()->score }} / {{ $homework->points }}
-						    @endif
-						    Due Date : {{ $homework->due_date }}	
-						    </h5>
-					    	
-                  </div>
-
-                 
-                  
-                  <div class="clearfix"></div>
-                  <b>Question : </b><br><br>
+<div class="panel panel-default question-panel" style="width: 100%;">
+    <div class="panel-heading">
+    	
+    	Problem {{ $index+1 }} | 
+	    @if(!$homework->isSolved())		
+	    	Points : {{ $homework->points }} 
+	    @else
+	    	Score : {{ $homework->userSolution()->score }} / {{ $homework->points }} 
+	    @endif
+	   <!-- Due Date : {{ $homework->due_date }} -->	
+	    
+    	
+    </div>
+	<div class="panel-body" >
+	    <b>Question : </b><br><br>
 	    <div id="homework-{{ $homework->id }}">
 			<div class="homework-question-{{ $homework->id }}">
 			{{ $homework->q }}
@@ -31,32 +26,34 @@
 		</div>	
 		<p id="answer-response-{{ $homework->id }}"></p>
 
-			<div class="float-left" id="solution-{{ $homework->id }}">
+		
+			
+		
+		
+	</div>
+	<div class="panel-footer">
+	   <div class="float-left" id="solution-{{ $homework->id }}">
 	     @if($homework->overDueDate())
 
-	     	<p class="mb-0"><b>The deadline for submission has passed!</b></p>
+	     	<b>The deadline for submission has passed!</b>
 
 	     @else
 
 	      @if(!$homework->hasAttempts() && !$homework->isSolved())
 
-	      	<p class="mb-0"><b>You don't Have any attempts left!</b></p>
+	      	<b>You don't Have any attempts left!</b>
 
 	      @else
 		     @if($homework->isSolved())
 
-		       <p class="mb-0"><b>Problem Solved in {{  3 - $homework->userSolution()->attempts }} attempts! Your answer : {{  $homework->userSolution()->answer }}</b></p>
+		       <b>Problem Solved in {{  3 - $homework->userSolution()->attempts }} attempts! Your answer : {{  $homework->userSolution()->answer }}</b>
 
 		     @else
-				<input type="text" id="answer-{{ $homework->id }}" class="form-control" placeholder="Your Answer" style="width:60%;display: inline-block;height: 33px;">
-				<button class="btn btn-sm btn-primary" onclick="submitAnswer({{$homework->id}})">Submit Answer</button>
+				<input type="text" id="answer-{{ $homework->id }}" class="form-control" placeholder="Your Answer" style="width:60%;display: inline-block;height: 40px;">
+				<button class="btn btn-dark btn-theme-colored btn-flat"  onclick="submitAnswer({{$homework->id}})">Submit Answer</button>
 			 @endif	
 		   @endif
 		 @endif  	 
 		</div>
-		
-
-                </div>
-    
-	
+	</div>
 </div>
