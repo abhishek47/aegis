25c0a5d4872891f7953dd54fa9eac3fc4bdb7426
abhishek47 <abhishekwani@hotmail.com>
2018-01-29 06:14:42 +0530

    forgot--password

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@aegisacademy.co.in'),
+        'name' => env('MAIL_FROM_NAME', 'Aegis Academy'),
     ],
 
     /*
