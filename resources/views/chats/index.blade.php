ef5bf4b2ca3ec3427876ef7ac4ec1e2f9fcb364a
abhishek47 <abhishekwani@hotmail.com>
2018-02-06 07:26:29 +0530

    design-revamped

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -48,6 +48,30 @@
 
 	<link rel="stylesheet" type="text/css" href="/css/chats.min.css">
 
+	<script type="text/x-mathjax-config">
+      MathJax.Hub.Config({
+      CommonHTML: { linebreaks: {automatic: true}},
+  SVG: { linebreaks: {automatic: true}},
+ 
+       "HTML-CSS": {
+       linebreaks: {automatic: true},
+    styles: {
+      ".MathJax_Display": {
+        width: null,
+        position: null,
+        display: "table"
+      }
+    }
+  },
+        showProcessingMessages: false,
+        tex2jax: { inlineMath: [['$','$'],['\\(','\\)']] },
+        TeX: { equationNumbers: {autoNumber: "AMS"}, extensions: ["AMSmath.js","AMSsymbols.js"] },
+
+      });
+    </script>
+    
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
 </head>
 
 <body>
