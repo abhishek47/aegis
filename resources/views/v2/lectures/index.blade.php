ba20b8300480646f36639b0d44ab736d6a5da629
abhishek47 <abhishekwani@hotmail.com>
2018-02-25 10:48:57 +0530

    latex

--- resources/views/v2/lectures/index.blade.php
+++ resources/views/v2/lectures/index.blade.php
@@ -20,13 +20,17 @@
            <div class="card card-shadow" style="width: 100%;cursor: pointer;" >
                  
                  <div class="card-body"> 
-                 <h2  class="title font-medium m-t-0"><a class="text-dark" href="/lectures/{{ $lecture->id }}">{{ $lecture->name }}</a></h2> 
-                  <div class="text-dark marked-input m-b-10" >   
-                     {{ $lecture->short_description }}
-                  </div>
-                  <p class="m-b-10 font-medium text-primary">Lecture on : {{ $lecture->date }}, {{ $lecture->start_time }}</p>
+                 
+                    <h2  class="title font-medium m-t-0"><a class="text-dark" href="/lectures/{{ $lecture->id }}">{{ $lecture->name }}</a></h2> 
+                    
+                    <div class="text-dark marked-input m-b-10" >   
+                       {{ $lecture->short_description }}..<a href="/lectures/{{ $looking->id }}">Read More</a>
+                    </div>
+                  
+                     <p class="m-b-10 font-medium text-primary">Lecture on : {{ $lecture->date }}, {{ $lecture->start_time }}</p>
 
-      <a target="_blank" href="{{ $lecture->link }}" class="btn btn-primary"><i class="fa fa-play-circle"></i> &nbsp; Join Class</a>
+                     <a target="_blank" href="{{ $lecture->link }}" class="btn btn-primary"><i class="fa fa-play-circle"></i> &nbsp; Join Class</a>
+                  
                   </div>
 
                   <div class="card-footer bg-danger" style="padding: 2px;"></div>
