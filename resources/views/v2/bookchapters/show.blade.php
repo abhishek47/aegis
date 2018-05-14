5ffbc097c47ff735e099c81e4f2ca2e2d325ada4
abhishek47 <operations@trumpetstechnologies.com>
2018-05-14 13:46:58 +0530

    changes

--- resources/views/v2/bookchapters/show.blade.php
+++ resources/views/v2/bookchapters/show.blade.php
@@ -5,7 +5,25 @@
 
 
 @section('content')
-
+  
+   <div class="banner-innerpage" style="background: url('/images/bg/bg-pattern.png'); background-size: cover;height: 340px;padding: 0;">
+               <div class="overlay" style="height: 340px;background: rgba(0, 173, 10, 0.8);">
+                <div class="container">
+                    <!-- Row  -->
+                    <div class="row ">
+                        <div class="col-md-3 m-t-40">
+                              <img src="{{ isset($chapter->book->image) ? Storage::disk('s3')->url($chapter->book->image) : 'http://via.placeholder.com/350x150' }}" style="width: 100%;height: 260px;box-shadow: 0px 0px 30px rgba(115, 128, 157, 0.4);">
+                        </div>
+                        <!-- Column -->
+                        <div class="col-md-8 aos-init aos-animate" data-aos="fade-down" data-aos-duration="1200">
+                            <h1 class="title p-t-30" style="">{{ $chapter->book->title }}<br>
+            <span style="font-size: 14px;color: #000;">by {{ $chapter->book->authors }}</span></h1>
+                            <h6 class="subtitle font-medium">{{ substr($chapter->book->short_description, 0, 350) }}...</h6> </div>
+                        <!-- Column -->
+                    </div>
+                </div>
+                </div>
+            </div>
 
 
 
@@ -16,7 +34,7 @@
      
 
          
-                  <h2><b>Chapter :</b> {{ $chapter->title }}</h2>
+                  <h2>{{ $chapter->title }}</h2>
                   <p class="course-description mt-10  markdown-body" id="classroom-summary" style="font-size: 16px;">{{ $chapter->description }}
                   </p>
                 
